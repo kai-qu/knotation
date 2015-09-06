cdd650b944d97d6544e0a650f7c1b9a50306317a
katherineye <kye@princeton.edu>
2015-09-06 06:29:49 -0400

    add outline for talk. add notes on trying to draw trefoil and generalize knot drawing. don't know how dowker will work

--- knot.hs
+++ knot.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 
+import Data.List
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
@@ -29,6 +30,107 @@ example :: Diagram B
 example = illustrateBézier c1 c2 x2
 
 ----------------------
+
+{- Types:
+tangles: e.g. 10***.3:1.1.1.3.-4......
+(polyhedra)
+
+Data:
+Basic tangles 0, infinity, -1, 1
+Basic polyhedron 1*
+
+Functions I need:
+twist :: nat -> Picture (or [Picture]?)
+
+reverseCrossings :: Picture -> Picture ([Picture] -> [Picture])
+
+rotate90CW :: Picture -> Picture ([Picture] -> [Picture])
+
+connectTangles :: Picture -> Picture -> Picture
+
+embedInPolyhedron :: Picture -> polyhedron -> Picture 
+(later, multiple tangles = [Picture] -> labeled polyhedron -> Picture)
+
+renderTangle :: tangle -> Picture
+
+renderKnot :: [tangle] -> polyhedron -> Picture
+or (conway notation = (Polyhedron, [Tangle]) where polyhedron is a number
+that indexes a list of the polyhedron pictures / draws the polyhedron around
+the tangles, and Tangle is a list of numbers with combinators like -, ,?
+-}
+
+type Twist = Int
+type Polyhedron = Int
+type ConwayNotation = ([Twist], Polyhedron)
+
+trefoil :: ConwayNotation
+trefoil = ([3], 1)
+
+-- goal for now
+figure8 :: ConwayNotation
+figure8 = ([2, 2], 1)
+
+stevedore :: ConwayNotation
+stevedore = ([4, 2], 1)
+
+six_2 :: ConwayNotation
+six_2 = ([3, 1, 2], 1)
+
+six_3 :: ConwayNotation
+six_3 = ([2, 1, 1, 2], 1)
+
+-----------
+
+-- Code to generate knots and filter out (lower-crossing knots / links / dups)
+
+-- given # crossings, produce all knots with that # crossings
+dowkers :: Int -> [[(Int, Int)]]
+dowkers n
+  | n <= 0 = []
+  | otherwise =
+  let (odds, evens) = (take n [1, 3 ..], take n [2, 4 ..]) in
+  map (zip odds) (permutations evens)
+
+partitions :: Int -> [[Int]]
+partitions n
+  | n <= 0 = [[]]
+  | otherwise =
+  let choose n = [1, 2 .. n] in
+  let subpartition y = map (y :) $ partitions (n - y) in
+  concatMap subpartition (choose n)
+
+-----------
+
+-- foo :: [Picture]
+-- foo = [Circle 200]
+
+-- embedIn :: Polyhedron -> [Picture] -> [Picture]
+-- embedIn polyhedron tanglePic = foo
+
+-- -- should probably return other stuff like where the relevant endpoints are
+-- renderTangle :: [Twist] -> [Picture]
+-- renderTangle t = foo
+-- -- let multiply a b = a :: [b] in {- is there an identity for mult? -}
+--              -- foldl multiply [0] . map renderTwist
+
+-- renderKnot :: ConwayNotation -> [Picture]
+-- renderKnot (tangle, polyhedron) = embedIn polyhedron $ renderTangle tangle
+
+-- -- TODO: commas, polyhedra
+
+-- initWorld :: [Picture]
+-- initWorld = renderKnot trefoil
+
+----------------------
+
+{- Notes on drawing:
+
+- Straight lines for Conway instead -- but how to prevent self-intersection?
+- Can a spline through every knot point (in order) work? But then how to articulate the crossings? 
+- Maybe a spline through each segment. But how to calculate the segments? And how to figure out the extra control points?
+- Twists: should only draw (n - 2) of them; the 2 crossings at the end need to be part of the splines that connect to other twists 
+
+To draw: trefoil, [2 1]; [2 1 2]; [3 3 3]... -}
 
 w = 1                           -- total width
 h = 4                      -- total height
@@ -59,7 +161,11 @@ twist n base =
       -- mkPath False ||| mkPath False
 
 cubicSplineEx :: Diagram B
-cubicSplineEx = (twist 5 (0, 0) ||| (twist 3 (5, 5) # rotateBy (1/4)))
+cubicSplineEx = twist 3 (0, 0) # rotateBy (1/4)
+              -- TODO: for multiple twists, endpoints + auto-placement +
+              -- the no-rotate method?
+              -- for figure-8      
+              -- (twist 2 (0, 0) ||| (twist 2 (0, 0) # rotateBy (1/4)))
                 # centerXY # pad 1.1
 
 main = mainWith cubicSplineEx
